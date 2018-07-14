64280b06cfe241edef06f16061da03f195d47cc5
Master-Owl <cheesytrevor@gmail.com>
2018-07-14 17:45:37 -0600

    Reformatting?

--- src/gui/startup/Main.java
+++ src/gui/startup/Main.java
@@ -9,18 +9,18 @@ import javafx.stage.Window;
 public class Main extends Application {
 
     @Override
-    public void start(Stage window) throws Exception{
+    public void start(Stage window) throws Exception {
         StartScreenController startScreenController = new StartScreenController();
         startScreenController.show(window);
     }
 
     public static void CloseActiveWindow(ActionEvent event) {
-        Stage window = (Stage)GetActiveWindow(event);
+        Stage window = (Stage) GetActiveWindow(event);
         window.close();
     }
 
     public static Window GetActiveWindow(ActionEvent event) {
-        Node node = (Node)event.getSource();
+        Node node = (Node) event.getSource();
         return node.getScene().getWindow();
     }
 
